452b9658b226682ae3069d369e319fb036b6dd63
abhinavrajput1111 <abhinav.r.1111@gmail.com>
2024-05-30 17:06:01 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Footer from "./Footer.jsx";
 import Quotes from "./Quotes2.jsx";
 import Navbar from "./Navbar.jsx";
 import "./App.css";
+import Randomquotes from "./Randomquotes.json";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Quotes />
+      <Quotes Randomquotes={Randomquotes} />
       <Footer />
     </>
   );
--- src/Quotes2.jsx
+++ src/Quotes2.jsx
@@ -1,26 +1,23 @@
-import Randomquotes from "./Randomquotes.json";
-
-function Quotes() {
+function Quotes(props) {
   return (
     <>
-      {Randomquotes.quotes.map((quote) => {
+      {/*console.log(props.Randomquotes.quotes)*/}
+      {props.Randomquotes.quotes.map((quote, index) => {
         return (
-          <>
-            <div
-              id="wrapper"
-              className="wrapper p-5 w-full h-auto border my-5 shadow-md bg-orange-50"
-            >
-              <div id="box" className="box">
-                <p className="text-center font-serif  text-2xl hover:text-blue-500 cursor-pointer">
-                  {" "}
-                  {quote.quote}{" "}
-                </p>
-                <p className="mt-3 text-center hover:text-amber-500">
-                  {quote.author}
-                </p>
-              </div>
+          <div
+            key={index}
+            id="wrapper"
+            className="wrapper p-5 w-full h-auto border my-5 shadow-md bg-orange-50"
+          >
+            <div id="box" className="box">
+              <p className="text-center font-serif text-2xl hover:text-blue-500 cursor-pointer">
+                {quote.quote}
+              </p>
+              <p className="mt-3 text-center hover:text-amber-500">
+                {quote.author}
+              </p>
             </div>
-          </>
+          </div>
         );
       })}
     </>
